ba1335c84a23192dddf4f9f2e63825337fcc77cf
Lincoln Oliver <lincolnolive@gmail.com>
2024-10-18 22:17:11 -0300

    Create get_report function and get the last report available

--- api_mercado_pago_report_generator.py
+++ api_mercado_pago_report_generator.py
@@ -1,12 +1,17 @@
 # %%
 import os
+import json
 import requests
+import pandas as pd
 from dotenv import load_dotenv
 
+# %%
 env_path = os.path.join(os.path.dirname(__file__),'.env')
 load_dotenv(dotenv_path=env_path)
 
 token = os.getenv('MERCADO_PAGO_TEST_TOKEN_SECRET')
+
+# %%
 
 def get_reports(token):
     headers = { 
@@ -17,13 +22,24 @@ def get_reports(token):
     response = requests.get('https://api.mercadopago.com/v1/account/settlement_report/list', headers=headers)
     return response.content
 
-# %%
-
-headers = {
+def get_report(token, report_name):
+    headers = {
     'Authorization': f'Bearer {token}'
-}
+    }
 
-response = requests.get('https://api.mercadopago.com/v1/account/settlement_report/settlement-report-450494286-2021-04-24-200025.csv', headers=headers)
-print(f'Status: {response.status_code}')
-print(response.content)
+    response = requests.get(f'https://api.mercadopago.com/v1/account/settlement_report/{report_name}', headers=headers)
+    return response.content
+
+# %%
+reports = get_reports(token).decode('utf-8')
+reports = json.loads(reports)
+df = pd.DataFrame(reports)
+
+# %%
+last_report = get_report(token, df['file_name'][0]).decode('utf-8')
+
+# Check how to make the last report to Data frame as it is text
+# data = pd.DataFrame(last_report)
+
+
 # %%
